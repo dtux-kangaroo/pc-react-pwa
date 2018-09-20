704423e7d4562a8f1a85d387b0f93cce6fdd49b7
yangyi <yhxyangyi@163.com>
2018-09-20 16:00:19 +0800

    pwa--init

---
+++ src/constants/apis.js
@@ -0,0 +1,17 @@
+export default {
+  /*---------------------home---------------------------*/
+  getHomeData: '/mock/homeData.json', // 获取总览数据
+  getUserData: '/mock/userData.json',      // 获取appname panel面板数据
+  getNavData: '/mock/navData.json',
+  getUserList:'/mock/userList.json',
+
+  /*---------------------searchTable---------------------------*/
+  getSearchTableData:'/mock/searchTableData.json',
+  getOptionData:'/mock/optionData.json',
+  saveTabelData:'/mock/success.json',
+  /*---------------------list---------------------------*/
+  getListData:'/mock/listData.json',
+
+
+  getDetailData:'/mock/detailData.json'
+}
